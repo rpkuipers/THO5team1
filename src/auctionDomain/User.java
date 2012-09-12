4be761f3156d2c8a3d97c05c27bf4e48efda6a9c
Kevin Clifford <k.clifftp@gmail.com>
2012-09-12 10:33:25 +0200

    User class

---
+++ src/auctionDomain/User.java
@@ -0,0 +1,68 @@
+package auctionDomain;
+
+public class User {
+	private int id;
+	private String name;
+	private String Address;
+	private String email;
+	private String phoneNumber;
+	private String username;
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
